c2cbffd9ba2f0d53537e5be5286759ea28d43aba
Dominik Horn <dominik.horn@tum.de>
2022-03-28 18:54:01 -0400

    sophisticate books experiment script

--- run-books-experiment.sh
+++ run-books-experiment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 TARGET_NAME="icdf_books_experiment"
-./build.sh ${TARGET_NAME} RELEASE
-cmake-build-release/src/${TARGET_NAME}
+MODE="DEBUG"
+LOWERCASE_MODE=$(echo "${MODE}" | awk '{print tolower($0)}')
+
+./build.sh ${TARGET_NAME} ${MODE}
+cmake-build-${LOWERCASE_MODE}/src/${TARGET_NAME}
